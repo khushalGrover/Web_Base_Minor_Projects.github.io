36037b2b8f6d2f1c28599e027a95d4d06ffc8a61
Khushal Grover <khushal15grover@gmail.com>
2022-12-10 13:49:33 +0530

    day 11 Done
    in this section elarn about keydown event lisner and key code, keycodes....

---
+++ Day_11_Event_Key_Codes/src/script.js
@@ -0,0 +1,23 @@
+const insert = document.getElementById('insert')
+window.addEventListener('keydown' , (e) => {
+    insert.innerHTML = `
+    <div class="key">
+            ${e.key === ' ' ? 'Space' : e.key}
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small>event.keycode</small>
+        </div>
+        <div class="key">
+            ${e.code}
+            <small>event.code</small>
+        </div>
+    `
+})
+
+// function e()
+// {
+//     console.log("trying to call");
+//     console.log(e);
+// }
