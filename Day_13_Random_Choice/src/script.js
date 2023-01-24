f0976f1edea02f0d21edd5c2620e124a46ea5a33
Khushal Grover <khushal15grover@gmail.com>
2023-01-25 00:14:16 +0530

    day 13 done
    random picker is ready

--- Day_13_Random_Choice/src/script.js
+++ Day_13_Random_Choice/src/script.js
@@ -4,11 +4,11 @@ const textArea = document.getElementById('textarea')
 // automatically focus on text area 
 textArea.focus()
 
-textArea.addEventListener('keyup', (e)=>{
+textArea.addEventListener('keyup', (e)=> {
     createTags(e.target.value)
 
-    if(e.key == 'Enter'){
-        setTimeout(() =>{
+    if(e.key == 'Enter') {
+        setTimeout(() => {
             e.target.value = ''
         }), 10
         randomSelect()
@@ -17,13 +17,13 @@ textArea.addEventListener('keyup', (e)=>{
  
 
 
-function createTags(input){
+function createTags(input) {
     // const tags = input.split(',') // spliting input(string) and assing into tags(array) when (,) is there
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim()) // use to remove space like 'd   ' to 'd' and '    ' to ignore
     
     tagEle.innerHTML = '' // clearing div with tags id
     
-    tags.forEach(tag =>{ // loop through all elements of tags array
+    tags.forEach(tag => { // loop through all elements of tags array
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')  // add tag class to span
         tagEl.innerText = tag       // span with tag class text is change to arrayValue
@@ -37,7 +37,41 @@ function createTags(input){
 }
 
 
+// call PickRandomTag functioin and give that random picking animation...
+function randomSelect() {
+    const times = 10
+    const interval = setInterval(() =>  {
+        const randomTag = pickRandomTag()
 
-function randomSelect(){
-    console.log("enter press");
+        highlightTag(randomTag)
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        },100)
+    },100);
+     
+    setTimeout(() => {
+        clearInterval(interval)
+        
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        })
+
+    }, times * 100)
+}
+
+// pick random tag 
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+
+function highlightTag(tag) {
+    tag.classList.add('active')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('active') 
 }
