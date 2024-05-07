961b60af365ffdb94e467b8d1f52a9c809d2c3d2
khushalGrover <khushal15grover@gmail.com>
2024-05-07 15:53:14 +0530

    pass generator working

--- Day_31_Password_Generator/src/script.js
+++ Day_31_Password_Generator/src/script.js
@@ -1,66 +1,105 @@
-const resultEl = document.getElementById('result') // error
-const upperCaseEl = document.getElementById('upperCase')
-const lowerCaseEl = document.getElementById('lowerCase')
-const symbolsEl = document.getElementById('symbols')
-const numberEl = document.getElementById('number')
-const lengthEl = document.getElementById('length')
-const copyEl = document.getElementById('copy')
-const generateEl = document.getElementById('generate')
+const resultElement = document.getElementById('result');
+const lengthElement = document.getElementById('length');
+const uppercaseElement = document.getElementById('uppercase');
+const lowercaseElement = document.getElementById('lowercase');
+const numbersElement = document.getElementById('number');
+const symbolsElement = document.getElementById('symbol');
+const generateElement = document.getElementById('generate');
+const clipboardElement = document.getElementById('clipboard');
+const msg = document.getElementById('msg');
 
-const randomFuncSelector = {
+const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
-    symbol: getRandomSymbol
-}
+    symbol: getRandomSymbol,
+};
 
-generateEl.addEventListener('click', () => {
-    const length = +lengthEl.value
-    const isLower = lowerCaseEl.checked
-    const isupper = upperCaseEl.checked
-    const isNumber = numberEl.checked
-    const isSymbols = symbolsEl.checked
-    // console.log(resultEl.innerText);    // error
-    resultEl.innerText = generatePassword( length, isLower, isupper, isNumber,isSymbols);
-})
+// adding an event listener to the clipboard button to copy the password to the clipboard
+clipboardElement.addEventListener('click', () => {
 
-
-function generatePassword(len, Low, Up, Num, Sym) {
-    let _password = ''
-    const typeCount = Low + Up + Num + Sym
-    const typeArr = [{Low},{Up},{Num},{Sym}].filter(item => Object.values(item)[0])
-
-    if(typeCount === 0) {
-        return ''
+    // storing the password in a variable
+    const password = resultElement.innerText;
+    // checking if the password is empty
+    if (!password) {
+        return;
     }
 
-    for(let i=0; i <len; i += typeCount) {
-        typeArr.forEach( type => {
-            const funcName = Object.keys(type)[0]
-            _password += randomFuncSelector[funcName]()
-        })
+    // copying the password to the clipboard
+    // Resource: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText
+    navigator.clipboard.writeText(password);
+    msg.innerText = 'Password copied to clipboard';
+    // Resetting the copied message after 4 seconds
+    setTimeout(() => {
+        msg.innerText = '';
+    }, 4000);
+});
+
+// adding an event listener to the generate button for creating a random string
+generateElement.addEventListener('click', () => {
+    const length = +lengthElement.value;
+    const hasLower = lowercaseElement.checked;
+    const hasUpper = uppercaseElement.checked;
+    const hasNumber = numbersElement.checked;
+    const hasSymbol = symbolsElement.checked;
+    // calling the generatePassword function to generate a random string and storing it in a variable
+    resultElement.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// function generatePassword to generate a random string
+function generatePassword(lower, upper, number, symbol, length) {
+    // a variable to store the final password
+    let generatedPassword = '';
+    // a variable to store the types of characters to be included in the password
+    const typesCount = lower + upper + number + symbol;
+    // an array to store the types of characters to be included in the password
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }, ].filter((item) => Object.values(item)[0]);
+
+    // checking if the typesCount is 0 then return 
+    if (typesCount === 0) {
+        return '';
     }
+    // looping through the length of the password as per the input from the user
+    for (let i = 0; i < length; i += typesCount) {
+        // looping through the typesArr
+        typesArr.forEach((type) => {
+            // getting the key of the type
+            const funcName = Object.keys(type)[0];
+            // adding the random character to the generatedPassword
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    // getting the final password by slicing the generatedPassword
+    const finalPassword = generatedPassword.slice(0, length);
 
-    const _finalPassword = _password.slice(0, len) 
-
-    return _finalPassword
-    console.log(typeArr);
+    // returning the final password
+    return finalPassword;
+}
+// function to get random lowercase letter
+function getRandomLower() {
+    return String.fromCharCode(
+        Math.floor(Math.random() * 26) + 97
+    );
 }
 
-
-function getRandomLower () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+// fucntion to get random uppercase letter
+function getRandomUpper() {
+    return String.fromCharCode(
+        Math.floor(Math.random() * 26) + 65
+    );
 }
 
-function getRandomUpper () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65  )
+// function to get a random number
+function getRandomNumber() {
+    return String.fromCharCode(
+        Math.floor(Math.random() * 10) + 48
+    );
 }
 
-function getRandomNumber () {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48  )
-}
-
+// function to get a random symbol
 function getRandomSymbol() {
-    const symbols = '!@#$%^&*()_+={}[]<>?,./'
-    return symbols[Math.floor(Math.random) * symbols.length]
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[
+        Math.floor(Math.random() * symbols.length)
+    ];
 }
